f1dc9dcef6dee9875007bde7e1eb0f6e5e872e81
Jonathanbloo <132208363+Jonathanbloo@users.noreply.github.com>
2025-04-06 23:46:48 +0200

    Mise à jour de page logement, responsive

--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -33,28 +33,30 @@ function Logements() {
         <Carousel imagesCarousel={logement.pictures} />
         {/* Affichage des détails du logement */}
         <div className="logement__details">
-          <div className="logement__informations">
-            <div>
-              <h1 className="logement__title">{logement.title}</h1>
-              <p className="logement__location">{logement.location}</p>
+          <div className="logement__description">
+            <div className="logement__informations">
+              <div>
+                <h1 className="logement__title">{logement.title}</h1>
+                <p className="logement__location">{logement.location}</p>
+              </div>
+              <div className="logement__tags">{tags}</div>
             </div>
-            <div className="logement__profil">
-              <p className="logement__profil__name">
-                <span>{logement.host.name.split(' ')[0]}</span>
-                <span>{logement.host.name.split(' ')[1]}</span>
-              </p>
-              <img
-                className="logement__profil__photo"
-                src={logement.host.picture}
-                alt="Photo de profil"
-              />
-            </div>
-          </div>
 
-          <div className="logement__tags__rating">
-            <div className="logement__tags">{tags}</div>
-            <div>
-              <Rating rating={logement.rating} />
+            <div className="logement__profil__rating">
+              <div className="logement__profil">
+                <p className="logement__profil__name">
+                  <span>{logement.host.name.split(' ')[0]}</span>
+                  <span>{logement.host.name.split(' ')[1]}</span>
+                </p>
+                <img
+                  className="logement__profil__photo"
+                  src={logement.host.picture}
+                  alt="Photo de profil"
+                />
+              </div>
+              <div>
+                <Rating rating={logement.rating} />
+              </div>
             </div>
           </div>
           <div className="logement__collapses">
